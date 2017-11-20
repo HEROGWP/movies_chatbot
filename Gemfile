f7bc5d638d04dd311fe2f62420825f8074d0b5d1
HEROGWP <pp820819@gmail.com>
2017-11-21 02:21:27 +0800

    加上排程

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ gem 'capistrano-passenger', :group => :development
 gem 'capistrano-bundler', '~> 1.2', :group => :development
 
 gem 'mysql2'
+gem 'whenever', :require => false
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ config/schedule.rb
@@ -0,0 +1,28 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+#
+# set :output, "/path/to/my/cron_log.log"
+#
+# every 2.hours do
+#   command "/usr/bin/some_great_command"
+#   runner "MyModel.some_method"
+#   rake "some:great:rake:task"
+# end
+#
+# every 4.days do
+#   runner "AnotherModel.prune_old_records"
+# end
+
+# Learn more: http://github.com/javan/whenever
+
+env :PATH, ENV['PATH'] #要用bundle時必須要加
+
+set :output, 'log/cron.log' #設定log的路徑
+
+every 13.minute do # 1.minute 1.day 1.week 1.month 1.year is also supported
+  rake 'movie:update_movies'
+end
