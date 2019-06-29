5697ff05624417c774e05eaf992165660c24315b
HEROGWP <pp820819@gmail.com>
2019-06-29 20:01:41 +0800

    - Add: send client message

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,4 +3,15 @@ class Client < ApplicationRecord
   belongs_to :movie, optional: true
   has_many :client_theater_ships, dependent: :destroy
   has_many :theaters, through: :client_theater_ships
+
+  def send_message(text)
+    Bot.deliver({
+      recipient: {
+        id: uid,
+      },
+      message: {
+        text: text,
+      },
+    }, access_token: ENV['ACCESS_TOKEN'])
+  end
 end
